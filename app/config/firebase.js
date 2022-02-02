51648e22c04a3b827c90f1d3811e58f6181c9d12
shvejan <shvejan2011@gmail.com>
2022-02-03 00:03:19 +0530

    download image in progress

--- app/config/firebase.js
+++ app/config/firebase.js
@@ -1,5 +1,5 @@
 import * as firebase from "firebase";
-
+import * as MediaLibrary from "expo-media-library";
 const firebaseConfig = {
   apiKey: "AIzaSyCaC5hSUJWnj6y1DLHIFpb-ya2uAK-uvNE",
   authDomain: "habit-tracker-8973e.firebaseapp.com",
@@ -16,9 +16,29 @@ if (!firebase.apps.length) {
 
 const storage = firebase.storage(firebase.apps[0]);
 
-export const uploadImage = async (image, name) => {
+export const uploadImage = async (image, path) => {
   const response = await fetch(image);
   const blob = await response.blob();
-  var ref = firebase.storage().ref().child(name);
+  var ref = firebase.storage().ref().child(path);
   return ref.put(blob);
+};
+export const downloadImage = (path, setimage) => {
+  let imageRef = firebase.storage().ref().child(path);
+  let image = imageRef
+    .getDownloadURL()
+    .then((url) => {
+      setimage(url);
+      console.log(url);
+    })
+    .catch((e) => console.log("getting downloadURL of image error => ", e));
+};
+export const saveImage = async (url) => {
+  const asset = await MediaLibrary.createAssetAsync(url);
+  MediaLibrary.createAlbumAsync("Expo", asset)
+    .then(() => {
+      console.log("Album created!");
+    })
+    .catch((error) => {
+      console.log("err", error);
+    });
 };
